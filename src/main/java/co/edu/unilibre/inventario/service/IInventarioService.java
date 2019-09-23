2a765f1632002cd02ccb35ed4c1770fec26592a4
ivanchuelo13 <ivanj.guerrac@unilibrebog.edu.co>
2019-09-23 00:47:24 -0500

    Se terminan de corregir los errores faltando el de inventario.service

--- src/main/java/co/edu/unilibre/inventario/service/IInventarioService.java
+++ src/main/java/co/edu/unilibre/inventario/service/IInventarioService.java
@@ -1,5 +1,10 @@
 package co.edu.unilibre.inventario.service;
 
-public class IInventarioService {
-	//agregar
+import java.util.List;
+
+import co.edu.unilibre.inventario.entity.Inventario;
+
+public interface IInventarioService {
+	public List<Inventario> getAllList();
+	public Inventario getById(Long id);
 }
--- src/main/java/co/edu/unilibre/inventario/service/impl/InventarioService.java
+++ src/main/java/co/edu/unilibre/inventario/service/impl/InventarioService.java
@@ -1,10 +1,26 @@
 package co.edu.unilibre.inventario.service.impl;
 
-import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import co.edu.unilibre.inventario.entity.Inventario;;
+import co.edu.unilibre.inventario.entity.Inventario;
+import co.edu.unilibre.inventario.repository.IInventaryRepository;
+import co.edu.unilibre.inventario.service.IInventarioService;
 
-public interface InventarioService {
-	public List<Inventario> getAllList();
-	public Inventario getById(Long id);
+@Service
+public class InventarioService implements IInventarioService{
+
+	@Autowired
+	private IInventaryRepository inventaryRepository;
+	
+	@Override
+	public List<Inventario> getAllList() {
+		return inventaryRepository.findAll();
+	}
+
+	@Override
+	public Inventario getById(Long id) {
+		return inventaryRepository.findById(id).get();
+	}
+	
 }
